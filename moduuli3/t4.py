758d39982a269b19c17f79ebde3d154ce6aaafb4
MightySeveri <severi.kerkka@gmail.com>
2025-01-22 14:29:26 +0200

    update

--- moduuli3/t4.py
+++ moduuli3/t4.py
@@ -1,5 +1,7 @@
 vuosi = int(input('syötä vuosiluku'))
-if vuosi % 4 == 0 or vuosi % 400 == 0:
+if vuosi % 4 == 0 and vuosi % 400 == 0:
+    print('vuosi on karannut')
+elif vuosi % 4 == 0 and vuosi % 400 != 0:
     print('vuosi on karannut')
 else:
     print('vuosi ei ole karannut')
---
+++ moduuli4/t1.py
@@ -0,0 +1,8 @@
+kierrokset = 0
+tehty = 0
+
+while tehty < 1000:
+    if kierrokset % 3 == 0:
+        print(kierrokset)
+    kierrokset += 1
+    tehty += 1
