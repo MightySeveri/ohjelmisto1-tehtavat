1e57ad766ccccb0fc18a692b3a91128712768f3e
MightySeveri <severi.kerkka@gmail.com>
2025-01-23 09:28:48 +0200

    update

--- moduuli4/t3.py
+++ moduuli4/t3.py
@@ -1,9 +1,13 @@
 luku = 6
 arva = input('arvaa luku')
+arva =int(arva)
 while luku != arva:
-    if arva < 6:
+    arva = int(input('arvaa luku'))
+    if arva < luku:
         print('liian pieni')
-    if arva > 6:
+    if arva > luku:
         print('liian suuri')
     if arva == luku:
         print('aivan oikein')
+
+#ohejlma kysyy ensimmäisellä kerralla kahdesti koska en keksinyt miten saan sen muuten loopattua
